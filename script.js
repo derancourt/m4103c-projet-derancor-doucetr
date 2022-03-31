559f5baa7a32d5d34bc6b9d58126e8f72dfd5001
derancour <remi.derancourt@etu.univ-grenoble-alpes.fr>
2022-03-31 10:00:32 +0200

    add listener

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 
-console.log('bon,jour');
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://age-of-empires-2-api.herokuapp.com/api/v1/structure/1')}`)
+.then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+})
+.then(data => console.log(data.contents));
+
+document.getElementById("rc").addEventListener('input', function(){
+  if(this.value.length == 5){
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://age-of-empires-2-api.herokuapp.com/api/v1/structure/1')}`)
+    .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Network response was not ok.')
+    })
+    .then(data => console.log(data.contents));
+  }
+});
