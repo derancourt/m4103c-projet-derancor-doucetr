3e0eed9ab91918465d4849409f194a0c023a6116
derancour <remi.derancourt@etu.univ-grenoble-alpes.fr>
2022-03-31 11:52:37 +0200

    modification index

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
-fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://age-of-empires-2-api.herokuapp.com/api/v1/structure/1')}`)
-.then(response => {
-        if (response.ok) return response.json()
-        throw new Error('Network response was not ok.')
-})
-.then(data => console.log(data.contents));
-
 document.getElementById("rc").addEventListener('input', function(){
   if(this.value.length == 5){
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://age-of-empires-2-api.herokuapp.com/api/v1/structure/1')}`)
+    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://age-of-empires-2-api.herokuapp.com/api/v1/structure/${this.value}`)}`)
     .then(response => {
-            if (response.ok) return response.json()
+            if (response.ok) return response.json().then((data) =>{
+              const json = data.contents;
+              const obj = JSON.parse(json);
+              let affichage = '<ul>';
+              for(let structure of obj){
+                affichage += `<li>${structure.id}</li>`;
+              }
+              affichage += '</ul>';
+              document.getElementById("bloc-resultats").innerHTML = affichage;
+            })
             throw new Error('Network response was not ok.')
     })
-    .then(data => console.log(data.contents));
+    .then(data => console.log(data));
   }
 });
