4b75aae828ede93d2eb5f147e16315919c250fbc
derancour <remi.derancourt@etu.univ-grenoble-alpes.fr>
2022-04-03 18:30:31 +0200

    ajout css

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("rc").addEventListener('input', function(){
   if(this.value.length == 5){
     fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://age-of-empires-2-api.herokuapp.com/api/v1/structure/${this.value}`)}`)
@@ -8,7 +7,7 @@ document.getElementById("rc").addEventListener('input', function(){
               const obj = JSON.parse(json);
               let affichage = '<ul>';
               for(let structure of obj){
-                affichage += `<li>${structure.id}</li>`;
+                affichage += `<li><div class="structures"><div class="name"><div class="st"><h1>Name : </h1><p>${structure.name}</p></div></div><div class="infos"><div class="info1"><div class="st2"><h2>Expansion :</h2><p>${structure.expansion}</p></div><div class="st2"><h2>Cost :</h2><p>${structure.cost}</p></div><div class="st2"><h2>Age :</h2><p>${structure.age}</p></div><div class="st2"><h2>Hit_Point :</h2><p>${structure.hit_point}</p></div></div><div class="info2"><div class=""><img src="" alt=""></div><div class="st2"><h2>Build_Time :</h2><p>${structure.build_time}</p></div><div class="st2"><h2>Armor :</h2><p>${structure.armor}</p></div><div class="st2"><h2>Special :</h2><p>"${structure.special}"</p></div></div></div></div></li>`;
               }
               affichage += '</ul>';
               document.getElementById("bloc-resultats").innerHTML = affichage;
